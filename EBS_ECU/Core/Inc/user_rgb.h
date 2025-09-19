96c2e9c466ce02ba8909be3e8062edd7510f2cc9
Yan-Haiyang <2411653676@qq.com>
2025-09-19 20:04:51 +0800

    完成与EBS逻辑电路看门狗芯片的交互

---
+++ EBS_ECU/Core/Inc/user_rgb.h
@@ -0,0 +1,54 @@
+/*
+ * user_led.h
+ *
+ *  Created on: Apr 22, 2025
+ *      Author: 24116
+ */
+
+#ifndef INC_USER_RGB_H_
+#define INC_USER_RGB_H_
+
+
+#include "main.h"
+extern volatile uint8_t blink_enabled;
+extern volatile uint8_t led_state;
+extern uint8_t EBS_Able_State;
+//0码和1码的定义，设置的时CCR寄存器的值
+//由于使用的思PWM输出模式1，计数值<CCR时，输出有效电平-高电平（CubeMX配置默认有效电平为高电平）
+#define CODE_1       (84)       //1码定时器计数次数，控制占空比为84/125 = 66%
+#define CODE_0       (42)       //0码定时器计数次数，控制占空比为42/125 = 33%
+
+//单个LED的颜色控制结构体
+typedef struct
+{
+	uint8_t R;
+	uint8_t G;
+	uint8_t B;
+}RGB_Color_TypeDef;
+
+#define Pixel_NUM 64  //LED数量宏定义
+
+ void Reset_Load(void); //该函数用于将数组最后24个数据变为0，代表RESET_code
+
+    //发送最终数组
+ void RGB_SendArray(void);
+
+ void RGB_Flush(void);  //刷新RGB显示
+
+
+void RGB_SetOne_Color(uint8_t LedId,RGB_Color_TypeDef Color);//给一个LED装载24个颜色数据码（0码和1码）
+
+
+//void RGB_RED(uint16_t Pixel_Len);  //显示红灯
+
+//控制多个LED显示相同的颜色
+void RGB_SetMore_Color(uint8_t head, uint8_t heal,RGB_Color_TypeDef color);
+void RGB_Show_64(void); //RGB写入函数
+void ASSI_Set_Blue(void);
+void ASSI_OFF(void);
+void ASSI_Blue_Stable(void);
+void ASSI_Blue_Blink(void);
+void ASSI_Yellow_Stable(void);
+void ASSI_Yellow_Blink(void);
+void WS2812_Update();
+#endif /* INC_USER_RGB_H_ */
